4782bb9a891d8e68b03f81417d474c6b5b382ea9
Ethan <41381482+ethandudu@users.noreply.github.com>
2024-02-12 09:14:39 +0100

    Addded setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="pyurlgrabber",
+    version="1.0",
+    author="Ethandudu",
+    description="Python URL Grabber tool",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
